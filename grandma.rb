4a29fdb76b968caf8ba50184583f909c668db7e1
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-17 16:21:47 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,5 +3,7 @@ def speak_to_grandma(phrase)
   "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != phrase.upcase
   "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase.upcase
+  "NO, NOT SINCE 1938!"
   end
 end
